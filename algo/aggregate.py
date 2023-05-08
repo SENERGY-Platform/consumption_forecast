c2d12127b18f77c278dbd1ce9dfbaa49c42142d0
Christoph Schrade <schrade.christoph@gmail.com>
2023-05-08 13:21:27 +0200

    add test print

--- algo/aggregate.py
+++ algo/aggregate.py
@@ -8,6 +8,8 @@ def aggregate(period, data_frame):
     if period=='week':
         data_frame['week'] = pd.Series(data_frame.index, index=data_frame.index).dt.isocalendar().week  # An extra column is added to the data frame that contains the data points' numbers of the week 
         weekly_grouper = data_frame.groupby(['week'])
+        for group in weekly_grouper:
+            print(group[1]['daily_consumption'])
         weekly_data_frame = pd.DataFrame([group[1]['daily_consumption'].sum() for group in weekly_grouper], index = [group[0] for group in weekly_grouper])
         weekly_time_series = darts.TimeSeries.from_dataframe(weekly_data_frame.iloc[1:], freq='W')
         return weekly_time_series
