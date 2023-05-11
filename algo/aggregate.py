ffb0e78b0d66c9d3273f60ffd994a733ade4b4ed
Christoph Schrade <schrade.christoph@gmail.com>
2023-05-11 12:11:12 +0200

    update monthly grouping

--- algo/aggregate.py
+++ algo/aggregate.py
@@ -13,13 +13,14 @@ def aggregate(period, data_frame):
         for group in weekly_grouper:
             print(group)
         weekly_data_frame = pd.DataFrame([group[1]['daily_consumption'].sum() for group in weekly_grouper if (group[1].index[-1].weekday()==6 and len(group)==7)], index = [group[1].index[-1] for group in weekly_grouper if (group[1].index[-1].weekday()==6 and len(group)==7)])
-        weekly_time_series = darts.TimeSeries.from_dataframe(weekly_data_frame.iloc[1:], freq='W')
+        weekly_time_series = darts.TimeSeries.from_dataframe(weekly_data_frame, freq='W')
         return weekly_time_series
     if period=='month':
         data_frame['month'] = data_frame.index.month  # An extra column is added to the data frame that contains the data points' numbers of the month 
+        data_frame['month'] = pd.DataFrame(data_frame.index.year, index = data_frame.index)[0].apply(lambda x:str(x)+"M")+data_frame['month'].apply(lambda x: str(x).zfill(2))
         monthly_grouper = data_frame.groupby(['month'])
         for group in monthly_grouper:
             print(group)
-        monthly_data_frame = pd.DataFrame([group[1]['daily_consumption'].sum() for group in monthly_grouper], index = [group[1].index[-1] for group in monthly_grouper])
-        monthly_time_series = darts.TimeSeries.from_dataframe(monthly_data_frame.iloc[1:], freq='M')
+        monthly_data_frame = pd.DataFrame([group[1]['daily_consumption'].sum() for group in monthly_grouper if (group[1].index[-1].is_month_end and len(group) >= 25)], index = [group[1].index[-1] for group in monthly_grouper (group[1].index[-1].is_month_end and len(group) >= 25)])
+        monthly_time_series = darts.TimeSeries.from_dataframe(monthly_data_frame, freq='M')
         return monthly_time_series
