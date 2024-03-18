2ed17304790c3c2edf9a526b4be011737338c573
Christoph Schrade <schrade.christoph@gmail.com>
2024-03-18 11:37:58 +0100

    adjust logs

--- operator_def.py
+++ operator_def.py
@@ -19,6 +19,7 @@ class CustomConfig(Config):
     prediction_length = 1
     add_time_covariates = False
     time_periods = ["D"]
+    logger_level = "info"
 
 class Operator(OperatorBase):
     __metaclass__ = abc.ABCMeta
@@ -33,7 +34,7 @@ class Operator(OperatorBase):
         self.initial_data = True
 
         self.periods = self.config.time_periods.split(',')
-        print(self.periods)
+        logger.info(self.periods)
         self.all_possible_periods = {'H', '4H', 'D', 'W', 'M', 'Y'}
         self.period_translation_dict = {'H': 'Hour', 'D': 'Day', 'W': 'Week', 'M': 'Month', 'Y': 'Year'}
         self.period_single_or_multi_output = {'H': 'H', 'D': '4H', 'W': 'D', 'M': 'D', 'Y': 'W'}
@@ -66,7 +67,7 @@ class Operator(OperatorBase):
     
     def run(self, data, selector='energy_func'):
         self.timestamp = todatetime(data['Time']).tz_localize(None)
-        print('energy: '+str(data['Consumption'])+'  '+'time: '+str(self.timestamp))
+        logger.debug('energy: '+str(data['Consumption'])+'  '+'time: '+str(self.timestamp))
 
         if self.initial_data:
             for period in self.all_possible_periods:
